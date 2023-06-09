ca8d0e1f9cf24ad97a220a8991dc229291f524c0
aydreas <andreas.sch4@gmail.com>
2023-06-09 17:23:58 +0200

    feat: moar data

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ function verifyConditions() {
 function generateNotes(_pluginConfig, { nextRelease }) {
   core.setOutput("new-release-published", "true");
   core.setOutput("new-release-version", nextRelease.version);
+  core.setOutput("new-release-type", nextRelease.type);
+  core.setOutput("new-release-head", nextRelease.head);
+  core.setOutput("new-release-tag", nextRelease.tag);
+  core.setOutput("new-release-channel", nextRelease.channel);
+}
+
+function prepare(_pluginConfig, { nextRelease }) {
+  core.setOutput("new-release-notes", nextRelease.notes);
 }
 
 module.exports = {
   verifyConditions,
   generateNotes,
+  prepare
 };
